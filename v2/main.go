3ea69fea84c019c9eca3e8b301114190e29231da
João Pedro <jpgomes06@hotmail.com>
2024-12-04 23:54:49 -0300

    Adding swagger

--- v2/main.go
+++ v2/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	_ "github.com/Jpgomes06/bank-notes/docs"
 	"github.com/Jpgomes06/bank-notes/presentation"
 	"github.com/Jpgomes06/bank-notes/usecase"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title			bank-notes
+// @version		1.0
+// @description	This is an API for a withdrawal system.
+// @termsOfService	http://swagger.io/terms/
 func main() {
 	calculator := usecase.NewBankNotesCalculator()
 	handler := presentation.NewWithdrawalHandler(calculator)
 	r := gin.Default()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.POST("/transaction", handler.HandleWithdrawal)
 	r.Run(":3000")
 }
--- v2/presentation/handle_withdrawal.go
+++ v2/presentation/handle_withdrawal.go
@@ -6,18 +6,34 @@ import (
 	"net/http"
 )
 
+// @Description Handles requests related to bank note transactions.
 type WithdrawalHandler struct {
 	Calculator usecase.BankNotesCalculator
 }
 
+// @Description Represents a withdrawal transaction with the requested amount.
 type Transaction struct {
+	// @Description The amount to withdraw.
+	// @Example 100
 	Amount int `json:"Amount" binding:"required"`
 }
 
+// @Summary      Create a new WithdrawalHandler
+// @Description  Initializes a new handler with the provided bank notes calculator.
+// @Success      200  {object}  WithdrawalHandler
 func NewWithdrawalHandler(calculator usecase.BankNotesCalculator) *WithdrawalHandler {
 	return &WithdrawalHandler{Calculator: calculator}
 }
 
+// @Summary      Handle a withdrawal request
+// @Description  Processes the withdrawal amount and calculates the required bank notes.
+// @Tags         Transactions
+// @Accept       json
+// @Produce      json
+// @Param        transaction  body      Transaction  true  "Transaction Data"
+// @Success      200          {object}  map[string]interface{}  "Successful response with bank notes"
+// @Failure      400          {object}  map[string]string       "Invalid request or error in calculation"
+// @Router       /transaction [post]
 func (u *WithdrawalHandler) HandleWithdrawal(c *gin.Context) {
 	var transaction Transaction
 	if err := c.ShouldBindJSON(&transaction); err != nil {
